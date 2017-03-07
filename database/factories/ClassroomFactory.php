a3630bbdde9f82234892a6ab188091f7b30deb4f
xiyusullos <i@xy-jit.cc>
2017-03-07 18:42:53 +0800

    set ClassrommFactory name attribute to null

--- database/factories/ClassroomFactory.php
+++ database/factories/ClassroomFactory.php
@@ -3,7 +3,7 @@
 $factory->define(App\Models\Classroom::class, function (Faker\Generator $faker) {
     return [
         'number' => $faker->unique()->regexify('[0-9]{4}'),
-        'name' => $faker->word,
+        'name' => null,//$faker->word,
         'location' => $faker->randomElement(['南区', '北区'])
             .$faker->numberBetween(1, 9) .'号楼',
         'square' => $faker->randomFloat(2, 30, 100),
